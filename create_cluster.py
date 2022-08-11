b1b5ebdfb9e01eb6e80b368a8563b536bb578743
aditya-singh-io <aditya.singh@weka.io>
2022-08-11 12:54:55 +0530

    Updated the main file

--- create_cluster.py
+++ create_cluster.py
@@ -5,149 +5,7 @@
 
 import os
 import time
-
-### Def
-#####
-####################    
-
-def default_cluster():
-	print("\n **********Automated tool created by @d!+y@ S!ng#*********** \n")
-	print("\n*************Entering Default Cluster formation Module******************\n")
-	option=input("Do you want to create Default cluster ? y/Yes/No : ")
-	if 'y' in option or 'Y' in option:
-		cluster_name=input("Enter the cluster name : Note: Don't use _ or Underscore :")
-		print("\nSuccess..! Cluster Creation in progress \n")
-		cmd_create="./teka lab provision --template m5-clients-instance.yaml {} --size=6 && ./teka install {}".format(cluster_name,cluster_name)
-		os.system(cmd_create)
-		print("Cluster created Successfully. ! Pls run details from Automation tool ")		
-		time.sleep(216000)
-		cmd_alive="./teka lab keepalive "+cluster_name
-		os.system(cmd_alive)
-		print("Keeping system Alive ")
-		time.sleep(216000)
-		print("keeping system Alive for the second time ")
-		os.system(cmd_alive)
-		
-	else:
-		cmd_create=input("Enter the teka command to create cluster e.g. ./teka lab provision --> : ")
-		os.system(cmd_create)
-		print("Cluster created Successfully. ! Pls run details from Automation tool ")		
-		time.sleep(216000)
-		cmd_alive="./teka lab keepalive "+cluster_name
-		os.system(cmd_alive)
-		print("Keeping system Alive ")
-		time.sleep(432000)
-		print("keeping system Alive for the second time ")
-		os.system(cmd_alive)
-		
-def particular_build():
-	print("\n **********Automated tool created by @d!+y@ S!ng#*********** \n")
-	print("\n*************Entering Specific build Cluster formation Module******************\n")
-	cluster_name=input("Enter the cluster name : ")
-	size=5
-	try:
-		size_inp=input("Enter the size of the backends to be needed. By default it is 5 : ")
-		size=int(size_inp)
-	except:
-		size=5
-	ver_in=input("Enter the build version you want ? (Optional) default: Latest Build  : ")
-	cmd=''
-	if len(ver_in)>3:
-		print("Installing cluster with below mentioned details ")
-		print("Cluster name "+cluster_name)		
-		print("Size of the BE ",size)
-		print("Build to be installed in the cluster "+ver_in)
-		cmd='./teka lab provision --template=stateless-clients-testing.yaml '+cluster_name+' --size '+str(size)+' --env=oci && ./teka install --from-version '+ver_in+' '+cluster_name
-	else:
-		print("Installing cluster with latest Default Build")
-		print("Installing cluster with below mentioned details ")
-		print("Cluster name "+cluster_name)		
-		print("Size of the BE ",size)
-		print("Build to be installed in the cluster : Latest build (Fetching by System) ")
-		cmd='./teka lab provision --template=stateless-clients-testing.yaml '+cluster_name+' --size '+str(size)+' --env=oci && ./teka install '+cluster_name
-	os.system(cmd)
-
-def mbc_cluster():
-	print("\n **********Automated tool created by @d!+y@ S!ng#*********** \n")
-	print("\n*************Entering MBC Cluster formation Module******************\n")
-	cluster_name=input("Enter the cluster name : ")
-	cmd='./teka lab provision '+cluster_name+' --size 6 --type i3.2xlarge --env oci --ionode-count 3 --mbc-installation=yes -f && ./teka install '+cluster_name
-	os.system(cmd)
-
-def tesla_fd_config():
-	print("\n **********Automated tool created by @d!+y@ S!ng#*********** \n")
-	print("\n*************Entering TESLA FD CONFIG cluster formation Module******************\n")
-	cluster_name=input("Enter the cluster name : ")
-	cmd="./teka lab provision "+cluster_name+" --size 8 --type i3.xlarge --env oci --hosts-per-failure-domain AUTO && ./teka install "+cluster_name
-	os.system(cmd)
-
-def checkout_3_13_dev_staging():
-	cmd_1='git checkout CI/3.13-dev-staging'
-	os.system(cmd_1)
-	time.sleep(10)
-	cmd_2='git status'
-	os.system(cmd_2)
-	time.sleep(1)
-	cmd_3='git pull --rebase'
-	os.system(cmd_3)
-	time.sleep(20)
-	cmd_4='git submodule update --init --recursive'
-	os.system(cmd_4)
-	time.sleep(20)
-	print("Command Ran Successfully")
-
-def checkout_3_14_dev_staging():
-	cmd_1='git checkout CI/3.14-dev-staging'
-	os.system(cmd_1)
-	time.sleep(10)
-	cmd_2='git status'
-	os.system(cmd_2)
-	time.sleep(1)
-	cmd_3='git pull --rebase'
-	os.system(cmd_3)
-	time.sleep(20)
-	cmd_4='git submodule update --init --recursive'
-	os.system(cmd_4)
-	time.sleep(20)
-	print("Command Ran Successfully")
-
-def checkout_4_0_dev():
-	cmd_1='git checkout CI/4.0-dev'
-	os.system(cmd_1)
-	time.sleep(3)
-	cmd_2='git status'
-	os.system(cmd_2)
-	time.sleep(1)
-	cmd_5="git stash"
-	time.sleep(5)
-	os.system(cmd_5)
-	cmd_3='git pull --rebase'
-	os.system(cmd_3)
-	time.sleep(10)
-	cmd_6="git stash pop"
-	os.system(cmd_6)
-	time.sleep(5)
-	cmd_4='git submodule update --init --recursive'
-	os.system(cmd_4)
-	time.sleep(20)
-	print("Command Ran Successfully")
-
-def checkout_particular():
-	input_branch=input('Enter the branch you want to checkout.( E.g: 4.0-dev-staging ) --> : ')
-	cmd_1='git checkout CI/'+input_branch
-	os.system(cmd_1)
-	time.sleep(10)
-	cmd_2='git status'
-	os.system(cmd_2)
-	time.sleep(20)
-	cmd_3='git pull --rebase'
-	os.system(cmd_3)
-	time.sleep(20)
-	cmd_4='git submodule update --init --recursive'
-	os.system(cmd_4)
-	time.sleep(20)
-	print("Command Ran Successfully")
-
+import aadi as func
 
 #######################
 ##########################
@@ -172,16 +30,16 @@ input_1=int(input("\nEnter the number from above list : "))
 input_checkout=0
 if input_1==1:
 	os.system(cmd_clear)
-	default_cluster()
+	func.default_cluster()
 elif input_1==2:
 	os.system(cmd_clear)
-	mbc_cluster()
+	func.mbc_cluster()
 elif input_1==3:
 	os.system(cmd_clear)
-	tesla_fd_config()
+	func.tesla_fd_config()
 elif input_1==4:
 	os.system(cmd_clear)
-	particular_build()
+	func.particular_build()
 elif input_1==5:
 	os.system(cmd_clear)
 	print("\n **********Automated tool created by @d!+y@ S!ng#*********** \n")
@@ -201,13 +59,13 @@ elif input_1==6:
 	if input_checkout==0:
 		exit()
 	elif input_checkout==1:
-		checkout_3_13_dev_staging()
+		func.checkout_3_13_dev_staging()
 	elif input_checkout==2:
-		checkout_3_14_dev_staging()
+		func.checkout_3_14_dev_staging()
 	elif input_checkout==3:
-		checkout_4_0_dev()
+		func.checkout_4_0_dev()
 	elif input_checkout==4:
-		checkout_particular()
+		func.checkout_particular()
 	elif input_checkout==5:
 		exit()
 else:
