de5b13555310a5a2a837f3b5232f44553ddd11b1
aditya-singh-io <aditya.singh@weka.io>
2022-08-17 16:43:33 +0530

    Modified code and added functionality

--- aadi.py
+++ aadi.py
@@ -14,10 +14,14 @@ def default_cluster():
 	print("\n **********Automated tool created by @d!+y@ S!ng#*********** \n")
 	print("\n*************Entering Default Cluster formation Module******************\n")
 	option=input("Do you want to create Default cluster ? y/Yes/No : ")
+	env='oci'
 	if 'y' in option or 'Y' in option:
-		cluster_name=input("Enter the cluster name : Note: Don't use _ or Underscore :")
+		cluster_name=input("Enter the cluster name : Note: Don't use _ or Underscore : ")
+		inp_env=input("Please enter Env oci/aws ? (Default: oci ) : ")
+		if len(env)>2:
+			env=inp_env
 		print("\nCluster Creation in progress. Please wait for 10-15 mins \n")
-		cmd_create="./teka lab provision --template m5-clients-instance.yaml {} --size=6 && ./teka install {}".format(cluster_name,cluster_name)
+		cmd_create="./teka lab provision --template default.yaml {} --size=6  --env={} && ./teka install {}".format(cluster_name,env,cluster_name)
 		os.system(cmd_create)
 		print("Cluster created Successfully. ! Pls run details from Automation tool ")		
 		time.sleep(216000)
@@ -52,19 +56,90 @@ def particular_build():
 		size=5
 	ver_in=input("Enter the build version you want ? (Optional) default: Latest Build  : ")
 	cmd=''
+	env='oci'
+	inp_env=input("Please enter Env oci/aws ? (Default: oci ) : ")
+	if len(env)>2:
+		env=inp_env
 	if len(ver_in)>3:
 		print("Installing cluster with below mentioned details ")
 		print("Cluster name : "+cluster_name)		
 		print("Size of the BE :",size)
 		print("Build to be installed in the cluster : "+ver_in)
-		cmd='./teka lab provision --template=stateless-clients-testing.yaml '+cluster_name+' --size '+str(size)+' --env=oci && ./teka install --from-version '+ver_in+' '+cluster_name
+		cmd='./teka lab provision --template=stateless-clients-testing.yaml '+cluster_name+' --size '+str(size)+' --env='+env+' && ./teka install --from-version '+ver_in+' '+cluster_name
 	else:
 		print("Installing cluster with latest Default Build")
 		print("Installing cluster with below mentioned details ")
 		print("Cluster name :"+cluster_name)		
 		print("Size of the BE :",size)
 		print("Build to be installed in the cluster : Latest build (Fetching by System) ")
-		cmd='./teka lab provision --template=stateless-clients-testing.yaml '+cluster_name+' --size '+str(size)+' --env=oci && ./teka install '+cluster_name
+		#cmd='./teka lab provision --template=adi.yaml '+cluster_name+' --size '+str(size)+' --env=aws && ./teka install '+cluster_name
+		cmd='./teka lab provision --template=stateless-clients-testing.yaml '+cluster_name+' --size '+str(size)+' --env='+env+' && ./teka install '+cluster_name
+	os.system(cmd)
+
+def particular_build_stateful():
+	print("\n **********Automated tool created by @d!+y@ S!ng#*********** \n")
+	print("\n*************Entering Specific build Cluster formation Module******************\n")
+	cluster_name=input("Enter the cluster name : ")
+	size=5
+	try:
+		size_inp=input("Enter the size of the backends to be needed. By default it is 5 : ")
+		size=int(size_inp)
+	except:
+		size=5
+	ver_in=input("Enter the build version you want ? (Optional) default: Latest Build  : ")
+	cmd=''
+	env='oci'
+	inp_env=input("Please enter Env oci/aws ? (Default: oci ) : ")
+	if len(env)>2:
+		env=inp_env
+	if len(ver_in)>3:
+		print("Installing cluster with below mentioned details ")
+		print("Cluster name : "+cluster_name)		
+		print("Size of the BE :",size)
+		print("Build to be installed in the cluster : "+ver_in)
+		cmd='./teka lab provision --template=m5-clients-instance.yaml '+cluster_name+' --size '+str(size)+' --env='+env+' && ./teka install --from-version '+ver_in+' '+cluster_name
+	else:
+		print("Installing cluster with latest Default Build")
+		print("Installing cluster with below mentioned details ")
+		print("Cluster name :"+cluster_name)		
+		print("Size of the BE :",size)
+		print("Build to be installed in the cluster : Latest build (Fetching by System) ")
+		cmd='./teka lab provision --template default.yaml '+cluster_name+' --size '+str(size)+' --env='+env+' && ./teka install '+cluster_name
+	os.system(cmd)
+
+def custom_details():
+	print("\n **********Automated tool created by @d!+y@ S!ng#*********** \n")
+	print("\n*************Entering End to end details for Cluster formation Module******************\n")
+	cluster_name=input("Enter the cluster name : ")
+	size=5
+	try:
+		size_inp=input("Enter the size of the backends to be needed. By default it is 5 : ")
+		size=int(size_inp)
+	except:
+		size=5
+	ver_in=input("Enter the build version you want ? (Optional) default: Latest Build  : ")
+	cmd=''
+	inp_env='oci'
+	env=input("Enter the environment aws/oci (Default: OCI): ")
+	if len(env)>2:
+		inp_env=env
+	template='stateless-clients-testing.yaml'
+	temp_inp=input("Enter the template (Default: stateless-clients-testing.yaml): ")
+	if len(temp_inp)>3:
+		template=temp_inp
+	if len(ver_in)>3:
+		print("Installing cluster with below mentioned details ")
+		print("Cluster name : "+cluster_name)
+		print("Size of the BE :",size)
+		print("Build to be installed in the cluster : "+ver_in)
+		cmd='./teka lab provision --template='+template+' '+cluster_name+' --size '+str(size)+' --env='+inp_env+' && ./teka install --from-version '+ver_in+' '+cluster_name
+	else:
+		print("Installing cluster with latest Default Build")
+		print("Installing cluster with below mentioned details ")
+		print("Cluster name :"+cluster_name)
+		print("Size of the BE :",size)
+		print("Build to be installed in the cluster : Latest build (Fetching by System) ")
+		cmd='./teka lab provision --template='+template+' '+cluster_name+' --size '+str(size)+' --env='+inp_env+' && ./teka install '+cluster_name
 	os.system(cmd)
 
 def mbc_cluster():
